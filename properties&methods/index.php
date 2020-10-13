0478a0cb9bebb34f0ca64e39f4e866b41df4611f
Biddut Bhowmik <54990169+bhowmik23@users.noreply.github.com>
2020-10-13 15:06:21 +0600

    properties & methods done

--- properties&methods/index.php
+++ properties&methods/index.php
@@ -1,12 +1,30 @@
 <?php
     class User {
         // properties methods
+        public $username = 'bhowmik';
+        public $email = 'bhowmik@gmail.com';
+
+        public function addFriend() {
+            return "$this->username added new friend";
+        }
     }
     // object
     $userOne = new User();
     $userTwo = new User();
 
-    echo 'The class is ' . get_class($userOne);
+    echo $userOne->username. '<br>';
+    echo $userOne->email. '<br>';
+    echo $userOne->addFriend(). '<br>';
+
+    $userTwo->username = 'biddut';
+    $userTwo->email = 'biddut@gmail.com';
+    
+    echo $userTwo->email. '<br>';
+    echo $userTwo->username. '<br>';
+    echo $userTwo->addFriend(). '<br>';
+
+    // print_r(get_class_vars('User'));
+    // print_r(get_class_methods('User'));
 ?>
 <!DOCTYPE html>
 <html lang="en">
