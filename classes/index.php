1c7a612b0934bd2497a965b4f1c26fa1d67d0949
Biddut Bhowmik <54990169+bhowmik23@users.noreply.github.com>
2020-10-13 14:44:21 +0600

    classes

---
+++ classes/index.php
@@ -0,0 +1,21 @@
+<?php
+    class User {
+        // properties methods
+    }
+    // object
+    $userOne = new User();
+    $userTwo = new User();
+
+    echo 'The class is ' . get_class($userOne);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP</title>
+</head>
+<body>
+    
+</body>
+</html>
